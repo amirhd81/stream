f903ab3c8b68ebf23a77a2b0f96dc8d73370a48a
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-05-03 20:49:35 +0330

    fdsfds

--- rec.py
+++ rec.py
@@ -2,66 +2,38 @@ from playwright.sync_api import sync_playwright
 from undetected_playwright import stealth_sync
 import sys
 
-chromiumPath = "/usr/bin/chromium-browser"   # your system chromium path
+chromiumPath = "/usr/bin/chromium-browser"
 
 def main():
     url = "https://discord.com/channels/1171584364723847230/1199031302725312643"
     
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            # user_data_dir="/root/strem/pro/Profile 41",
-            # storage_state="state.json",
-            # record_video_dir="videos/",
-            # record_video_size={"width": 640, "height": 480},
             headless=True,
-            executable_path=chromiumPath,   # USE SYSTEM CHROMIUM
+            executable_path=chromiumPath,
             args=["--no-sandbox", "--disable-gpu"],
         )
 
-        context = browser.new_context({
-            storage_state: "state.json",
-            record_video_dir: "videos/",
-            record_video_size: {"width": 640, "height": 480},
-        });
-
+        context = browser.new_context(
+            storage_state="state.json",
+            record_video_dir="videos/",
+            record_video_size={"width": 640, "height": 480},
+        )
     
         page = context.new_page()
-
         stealth_sync(page)
-
 
         page.goto(url, wait_until="domcontentloaded")
         page.wait_for_timeout(2000)
-        # page.locator("input#uid_10").fill("amir.hd.davoudi1381@gmail.com")
-        # page.locator("input#uid_12").fill("@Lost4815162342")
-        # page.locator("button[type='submit']").click()
-        # page.wait_for_timeout(2000)
-        # page.wait_for_timeout(3000)
-        # page.locator("button.buttonOver18").nth(1).click()
-        # page.wait_for_timeout(2000)
-        
-        # page.locator("button.cbPrimaryCTA").click()
-        # page.wait_for_timeout(4000)
-        
-        # page.locator("input#searchInput").fill(search)
-        
-        # page.keyboard.press("Enter")
-        # page.wait_for_timeout(4000)
 
-        # page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
-        # page.wait_for_timeout(1500)
-
-        # page.wait_for_timeout(2000)
-        # page.screenshot(path="page.png", full_page=True)
-        co = page.content()
+        html = page.content()
 
         with open("div.html", "w") as f:
-            f.write(str(co))
+            f.write(html)
 
         context.close()
         browser.close()
 
 if __name__ == "__main__":
     search = sys.argv[1]
-
     main()
