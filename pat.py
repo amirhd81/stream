232dcd13e5f1f72ef84ada4152db94d57bbe1733
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-05-11 16:21:19 +0330

    fdsfs

--- pat.py
+++ pat.py
@@ -31,7 +31,7 @@ def download_video(url, height):
 
     format_str = f"bestvideo[height<={height}]+bestaudio/best[height<={height}]"
 
-    run(f"curl -H 'Referer: https://ln5.sync.com/' {url}")
+    run(f"curl -H 'Referer: https://ln5.sync.com/' '{url}'")
     
 
     # run(f"yt-dlp -4 --add-header 'Referer: https://www.patreon.com/' --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4  -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' {url}")
