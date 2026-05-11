b01b1a262fd42b752660d3cfe717e213f7a15566
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-05-11 16:16:40 +0330

    fdsfs

--- pat.py
+++ pat.py
@@ -34,7 +34,7 @@ def download_video(url, height):
     # run(f"wget {url}")
 
     # run(f"yt-dlp -4 --add-header 'Referer: https://www.patreon.com/' --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4  -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' {url}")
-    run(f"yt-dlp -4 -N 8 --concurrent-fragments 8    --add-header 'Referer: https://ln5.sync.com/'  --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4 -f 480p -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' '{url}'")
+    run(f"yt-dlp -4 --add-header 'Referer: https://ln5.sync.com/' -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' '{url}'")
 
 
 def split_rar():
