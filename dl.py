7f534b566d3a8df704875da766c753e54a3df23d
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-05-13 01:25:35 +0330

    fdsfs

--- dl.py
+++ dl.py
@@ -41,7 +41,7 @@ def download_video(url, height):
     format_str = f"bestvideo[height<={height}]+bestaudio/best[height<={height}]"
 
     # run(f"yt-dlp -4 --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4   --merge-output-format 'mp4' -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' {url}")
-    run(f"yt-dlp -4 --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4   --merge-output-format 'mp4' -f '{format_str}' -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' {url}")
+    run(f"yt-dlp -4 --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4   --merge-output-format 'mp4' -f '{format_str}' -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' '{url}'")
 
     # opts = {
     #     "format": format_str,
