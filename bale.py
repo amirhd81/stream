a1995fcf6325fab8a79e1cc67be6d8c6c30a7916
Amirhossein <77074643+amirhd81@users.noreply.github.com>
2026-05-13 02:01:11 +0330

    njhjh

--- bale.py
+++ bale.py
@@ -27,12 +27,32 @@ def run(cmd, cwd=None):
     
 def download_video(url, height):
     print(f"📥 Starting download at {height}p...")
+    
     format_str = f"bestvideo[height<={height}]+bestaudio/best[height<={height}]"
+    
+    log = open("/root/strem/yt.log", "a")
 
-    run("which yt-dlp")
-    run("which aria2c")
+    subprocess.Popen(
+        [    
+            "yt-dlp",
+            "-4",
+            "--downloader",
+            f"[m3u8]/usr/bin/aria2c",
+            "--downloader-args",
+            "aria2c:-x:16:-k:1M:-4",
+            "--merge-output-format",
+            "mp4",
+            "-f",
+            format_str,
+            "-o",
+            os.path.join(DOWNLOAD_DIR, "video.%(ext)s"),
+            url
+        ],
+        stdout=log,
+        stderr=log
+    )
 
-    run(f"yt-dlp -4 --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4   --merge-output-format 'mp4' -f '{format_str}' -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' '{url}'")
+    # run(f"yt-dlp -4 --downloader [m3u8]aria2c --downloader-args aria2c:-x:16:-k:1M:-4   --merge-output-format 'mp4' -f '{format_str}' -o '{os.path.join(DOWNLOAD_DIR, "video.%(ext)s")}' '{url}'")
 
 
 def send_message(chat_id, text):
